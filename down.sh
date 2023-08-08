012ab1b8c68262f9529586d5843aa81f365d3583
1kst <fxcktg@gmail.com>
2023-08-08 15:27:04 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 urls=(
-    "https://download-cdn.jetbrains.com/idea/ideaIU-2023.2.exe"
-    "https://download-cdn.jetbrains.com/webstorm/WebStorm-2023.2.exe"
-    "https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
-    "https://autopatchcn.yuanshen.com/client_app/download/launcher/20230625145853_3mVP4q9cR5z7F88A/mihoyo/yuanshen_setup_20230619213504.exe"
+"https://download-cdn.jetbrains.com/idea/ideaIU-2023.2.exe"
+"https://download-cdn.jetbrains.com/webstorm/WebStorm-2023.2.exe"
+"https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
+"https://autopatchcn.yuanshen.com/client_app/download/launcher/20230625145853_3mVP4q9cR5z7F88A/mihoyo/yuanshen_setup_20230619213504.exe"
 )
 
 output="/dev/null"
 
 while true; do
-    for url in "${urls[@]}"; do
-        echo "Downloading file: $url"
-        curl -o "$output" "$url"
-    done
+for url in "${urls[@]}"; do
+echo "Downloading file: url" curl --limit-rate 200M -o "output" "$url"
+done
 done
