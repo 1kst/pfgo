2a874427472f2df95bc7387f56dcfee73860bebd
1kst <fxcktg@gmail.com>
2023-08-08 15:16:30 +0800

    Update down.sh

--- down.sh
+++ down.sh
@@ -4,5 +4,7 @@ url="https://software-static.download.prss.microsoft.com/dbazure/988969d5-f34g-4
 
 while true
 do
-    wget -q -O /dev/null "$url"
+    echo "Downloading file..."
+    wget --progress=bar:force -q "$url" 2>&1 | tee /dev/tty
+    echo "Download complete."
 done
